41a4915ae88b99d97968cc2a704285f712ea4e28
ahmed-eficta <ahmed.eficta@gmail.com>
2026-03-18 05:04:04 +0200

    Refactor sports area layout in newspaper view for conditional blog rendering
    This commit enhances the sports area layout in the newspaper view by implementing conditional rendering for the left blog section. The changes ensure that the blog content is only displayed when available, improving the user experience by preventing empty sections and maintaining a clean layout.

--- core/plugins/PageBuilder/views/tenant/newspaper/sports-area.blade.php
+++ core/plugins/PageBuilder/views/tenant/newspaper/sports-area.blade.php
@@ -13,37 +13,39 @@
                     <a href="{{$data['view_more_url']}}" class="viewMore_btn">{{__('View More')}} <i class="las la-arrow-right"></i></a>
                 </div>
                 <div class="row g-4 mt-1">
-                    <div class="col-md-8">
-                        <div class="newspaper_sports newspaper_sports_bg">
-                            <div class="newspaper_sports__thumb">
-                                <a href="{{ route('tenant.frontend.blog.single',$left_blog->slug) }}">
-                                    {!! render_image_markup_by_attachment_id($left_blog->image) !!}
-                                </a>
-                            </div>
-                            <div class="newspaper_sports__contents pt-4">
-                                <div class="newspaper_sports__tag">
-                                    <a href="javascript:void(0)" class="newspaper_sports__tag__item">
-                                        <div class="newspaper_sports__tag__item__thumb">
-                                            <img src="{{global_asset('assets/tenant/frontend/themes/img/newspaper/latest/newspaper_author1.jpg')}}" alt="authorImg">
-                                        </div>
-                                        <span class="newspaper_sports__tag__item__title">{{$left_blog->author}}</span>
-                                    </a>
-                                    <a href="javascript:void(0)" class="newspaper_sports__tag__item">
-                                        <div class="newspaper_sports__tag__item__icon"><i class="las la-clock"></i></div>
-                                        <span class="newspaper_sports__tag__item__title">{{ date('d M Y',strtotime($left_blog->created_at)) }}</span>
-                                    </a>
-                                    <a href="javascript:void(0)" class="newspaper_sports__tag__item">
-                                        <div class="newspaper_sports__tag__item__icon"><i class="las la-comments"></i></div>
-                                        <span class="newspaper_sports__tag__item__title">{{$left_blog->comments?->count()}}</span>
+                    @if(!empty($left_blog))
+                        <div class="col-md-8">
+                            <div class="newspaper_sports newspaper_sports_bg">
+                                <div class="newspaper_sports__thumb">
+                                    <a href="{{ route('tenant.frontend.blog.single',$left_blog->slug) }}">
+                                        {!! render_image_markup_by_attachment_id($left_blog->image) !!}
                                     </a>
                                 </div>
-                                <h4 class="newspaper_sports__title style-02 mt-3"><a href="javascript:void(0)">{{$left_blog->title}}</a></h4>
-                                <p class="newspaper_sports__para style-02 mt-3">
-                                {!! $left_blog->excerpt ?? $strip_tags(\Illuminate\Support\Str::words($left_blog->blog_content,35)) !!}
-                                </p>
+                                <div class="newspaper_sports__contents pt-4">
+                                    <div class="newspaper_sports__tag">
+                                        <a href="javascript:void(0)" class="newspaper_sports__tag__item">
+                                            <div class="newspaper_sports__tag__item__thumb">
+                                                <img src="{{global_asset('assets/tenant/frontend/themes/img/newspaper/latest/newspaper_author1.jpg')}}" alt="authorImg">
+                                            </div>
+                                            <span class="newspaper_sports__tag__item__title">{{$left_blog->author}}</span>
+                                        </a>
+                                        <a href="javascript:void(0)" class="newspaper_sports__tag__item">
+                                            <div class="newspaper_sports__tag__item__icon"><i class="las la-clock"></i></div>
+                                            <span class="newspaper_sports__tag__item__title">{{ date('d M Y',strtotime($left_blog->created_at)) }}</span>
+                                        </a>
+                                        <a href="javascript:void(0)" class="newspaper_sports__tag__item">
+                                            <div class="newspaper_sports__tag__item__icon"><i class="las la-comments"></i></div>
+                                            <span class="newspaper_sports__tag__item__title">{{$left_blog->comments?->count()}}</span>
+                                        </a>
+                                    </div>
+                                    <h4 class="newspaper_sports__title style-02 mt-3"><a href="javascript:void(0)">{{$left_blog->title}}</a></h4>
+                                    <p class="newspaper_sports__para style-02 mt-3">
+                                    {!! $left_blog->excerpt ?? $strip_tags(\Illuminate\Support\Str::words($left_blog->blog_content,35)) !!}
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
                     <div class="col-md-4">
                         <div class="row g-4">
                             @foreach($blogs as $data)
